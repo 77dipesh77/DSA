8471b363fcacb77f3db26bb86fba65ed3229fc25
77dipesh77 <155010574+77dipesh77@users.noreply.github.com>
2025-08-09 21:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        for(int i = 0; i < 31; i++) {
+            int power = pow(2,i);
+            if (n == power) return true;
+        }
+            return false;
+    }
+};
