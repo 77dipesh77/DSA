eb9087973c7ad4acad1dfc1a42532de19bb592ed
77dipesh77 <155010574+77dipesh77@users.noreply.github.com>
2026-01-05 22:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (6.48%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -4,16 +4,17 @@ public:
 
         int r = 0;
         int l = 0;
-        long long subs = 0;
-        int MOD = 1e9 + 7;
+        int ans = 0;
+        int mod = 1e9 + 7;
 
         while(r < s.size()) {
             if(s[r] == '0') {
                 l = r+1;
             }
-            subs = (subs + (r-l+1)) % MOD;
+            ans = (ans + (r-l+1)) % mod;
             r++;
-        }        
-        return subs;
+        }
+        return ans;
+        
     }
 };
