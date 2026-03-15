3fbb8e11a81904784c093dc0842a35e2243bb620
77dipesh77 <155010574+77dipesh77@users.noreply.github.com>
2026-03-15 19:26:03 +0530

    Time: 32 ms (85.09%) | Memory: 88.7 MB (12.34%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
 
+        map<int,int> mpp;
         int l = 0;
         int r = 0;
-        map<int,int> mpp;
-        int maxi = 0;
-        
+
+        int maxi  = 0;
+
         while(r < fruits.size()) {
             mpp[fruits[r]]++;
+
             while(mpp.size() > 2) {
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
@@ -16,7 +18,7 @@ public:
             }
             maxi = max(maxi,r-l+1);
             r++;
-        }
+        }  
         return maxi;
         
     }
