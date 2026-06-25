2f43b577d0c20b38519d84dbbb1774ff424f9771
77dipesh77 <155010574+77dipesh77@users.noreply.github.com>
2026-06-25 17:15:20 +0530

    Time: 27 ms (90.05%) | Memory: 89 MB (7.50%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -5,20 +5,19 @@ public:
         map<int,int> mpp;
         int l = 0;
         int r = 0;
-
-        int maxi  = 0;
+        int maxi = 0;
 
         while(r < fruits.size()) {
             mpp[fruits[r]]++;
 
-            if(mpp.size() > 2) {
+            while(mpp.size() > 2) {
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
                 l++;
             }
             maxi = max(maxi,r-l+1);
             r++;
-        }  
+        }
         return maxi;
         
     }
